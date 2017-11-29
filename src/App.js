a1215b5459db8b0ecb351f7f8728e9fb13c2a736
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-11-29 14:59:10 +1000

    Added date/time to tasks, changed tasks to an object from Array, styled elements

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ class App extends Component {
 
   // Set state for tasks
   state = {
-    tasks: ['Do the washing', 'Walk the dog'],
+    tasks: [
+      { job: 'Do the washing', dateTime: "12/03/2017, 2:45:00 PM" },
+      { job: 'Walk the dog', dateTime: "11/29/2017, 6:30:00 AM" }
+    ],
     searchPhrase: ''
   }
 
@@ -27,9 +30,9 @@ class App extends Component {
     const currentTasks = [...this.state.tasks];
 
 
-    if (this.state.searchPhrase !== '' && !this.state.tasks.includes(this.state.searchPhrase)) {
+    if (this.state.searchPhrase !== '' && !this.state.tasks.filter(task => task.job === this.state.searchPhrase).length) {
       // Add new tasks to list of tasks
-      currentTasks.unshift(this.state.searchPhrase);
+      currentTasks.unshift({job: this.state.searchPhrase, dateTime: new Date().toLocaleString()});
 
       // Update the state with the new tasks
       this.setState({
@@ -42,6 +45,18 @@ class App extends Component {
     // Reset the search phrase to an empty string
 
   }
+
+  // removeTask = (event) => {
+  //   event.preventDefault();
+
+  //   const currentTasks = [...this.state.tasks];
+
+  //   currentTasks.shift();
+
+  //   this.setState({
+  //     tasks: currentTasks
+  //   })
+  // }
 
   // Render component
   render() {
@@ -56,7 +71,7 @@ class App extends Component {
       totalIncomplete={tasks.length} 
       title='INCOMPLETE' 
       />
-      <form onSubmit={this.addTask}>
+      <form className="Search" onSubmit={this.addTask}>
         <Input 
         primary 
         placeholder='search / add a todo!' 
@@ -67,8 +82,10 @@ class App extends Component {
 
       {
         tasks
-          .filter(myTask => myTask.includes(searchPhrase))
-            .map(myTask => <Notification warning>{myTask}</Notification>)
+          .filter(myTask => myTask.job.includes(searchPhrase))
+            .map(myTask =>
+                <Notification className="text-white" warning>{myTask.job}<br />{myTask.dateTime}</Notification>
+            )
       }
 
       </div>
